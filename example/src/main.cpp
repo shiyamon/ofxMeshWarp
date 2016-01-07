f2102e79ab174034ea82a0395a0b4c23d3333b34
吉田真也 <yoshida@mtblanc.jp>
2016-01-07 23:56:48 +0900

    Update example project.

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -3,7 +3,20 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    
+    bool checkGL4 = true;
+    
+    if(checkGL4) {
+        
+        ofGLWindowSettings s;
+        s.width = 1024;
+        s.height = 768;
+        s.setGLVersion(4, 1);
+        ofCreateWindow(s);
+    }
+    else {
+        ofSetupOpenGL(1024,768,OF_WINDOW);
+    }
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
